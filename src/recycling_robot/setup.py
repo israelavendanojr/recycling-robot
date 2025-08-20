6d128e54a52706801072d120ce8b405b3242efd0
israelavendanojr <iajr0207@gmail.com>
2025-08-19 17:58:01 -0700

    Package Files + Message, Service, Action Definitions

---
+++ src/recycling_robot/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'recycling_robot'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*launch.[pxy]'))),
+        # Include message definitions
+        (os.path.join('share', package_name, 'msg'),
+            glob(os.path.join('msg', '*.msg'))),
+        # Include service definitions
+        (os.path.join('share', package_name, 'srv'),
+            glob(os.path.join('srv', '*.srv'))),
+        # Include action definitions
+        (os.path.join('share', package_name, 'action'),
+            glob(os.path.join('action', '*.action'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Israel Avendano Jr',
+    maintainer_email='israelavendanojr@gmail.com',
+    description='AI-powered recycling robot with computer vision classification and physical sorting',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # Core nodes
+            'camera_node = recycling_robot.nodes.camera_node:main',
+            'classifier_node = recycling_robot.nodes.classifier_node:main',
+            'sorting_node = recycling_robot.nodes.sorting_node:main',
+            
+            # Support nodes
+            'web_bridge_node = recycling_robot.nodes.web_bridge_node:main',
+            'stats_monitor_node = recycling_robot.nodes.stats_monitor_node:main',
+            'safety_node = recycling_robot.nodes.safety_node:main',
+            
+            # Utilities
+            'system_launcher = recycling_robot.launch.system_launcher:main',
+        ],
+    },
+)
